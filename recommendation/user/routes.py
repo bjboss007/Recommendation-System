a58f37905d164746b028d84d62ba33c23da668c9
Habib <habib.muhammad@longbridgetech.com>
2019-12-13 21:39:47 +0100

    first commit

--- recommendation/user/routes.py
+++ recommendation/user/routes.py
@@ -5,7 +5,9 @@ from recommendation import bcrypt, db
 from .forms import RegistrationForm, LoginForm, UpdateForm, SubjectForm
 from .questions import questions
 import os
-
+from pickle import load
+import numpy as np
+from recommendation.utils import mapping, correctForm
 
 
 users = Blueprint('users', __name__)
@@ -23,8 +25,6 @@ def login():
         if user and bcrypt.check_password_hash(user.password,form.password.data):
             login_user(user, remember = form.remember.data)
             next_page = request.args.get('next')
-            # flash(f'Login successful ','success')
-            # flash(f'Please edit your profile to complete the registration', 'info')
             return redirect(next_page) if next_page else redirect(url_for('users.update'))
         else:
             flash(f'Login Unsuccessful, Please check your email and password ','danger')
@@ -71,7 +71,6 @@ def subjects(arm):
 @users.route('/users/update', methods = ['GET','POST'])
 @login_required
 def update():
-    
     if request.method == "POST":
         entries = list(request.form.items())
         subjects = entries[2:len(entries)-2]
@@ -81,23 +80,14 @@ def update():
         user_info.age = request.form["age"]
         user_info.career = request.form["career"]
         user_info.iq = 0
-            
         for subject in subjects:
-            print(subject)
-            
-            sub = Subject.query.filter_by(name = subject[0]).first()
-            print(sub.id)
-            
+            sub = Subject.query.filter_by(name = subject[0]).first()        
             sub_rating = Subjectrating(subject_id = sub.id, rating = subject[1])
             db.session.add(sub_rating)
             user_info.subjects.append(sub_rating)
-        print(user_info.subjects)
-
         current_user.userinfo = user_info
-        
         db.session.add(user_info)
         db.session.commit()
-        
         return redirect(url_for('users.question'))
     return render_template('test.html', title = 'Update',  user = current_user)
 
@@ -107,39 +97,22 @@ def logout():
     logout_user()
     return redirect(url_for('users.login'))
 
-
 @users.route('/users/account', methods = ['GET','POST'])
 @login_required
 def account():
-    form =  UpdateForm()
-    
-    if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            filename = os.path.join('http://localhost:5000/static/images',picture_file)
-            current_user.image_file = filename
-    
-        current_user.email = form.email.data
-        current_user.username = form.username.data
-        db.session.commit()
-        flash(f'Account Updated successfully','success')
-        return redirect(url_for('users.account'))
-    elif request.method == 'GET':
-        form.email.data = current_user.email
-        form.username.data = current_user.username 
-    return render_template("account.html", title="Account", form = form, user = current_user)
-
-
-@users.route('/reset_request', methods = ['GET','POST'])
-def reset_request():
-    return render_template('login.html', title = 'Login', form = form)
+    user = User.query.filter_by(username = current_user.username).first()
+    model_data = correctForm(user)
+    # data = np.array(model_data, dtype = np.float32).reshape(1,-1)
+    learner = load(open("../../../original.pkl", 'rb'))
+    prediction = learner.predict(model_data)
+    recom_course = mapping(prediction[0])
+    return render_template("account.html", title="Account", user = current_user, recom_course = recom_course)
 
 
 @users.route("/question", methods = ['GET','POST'])
 def question():
     proposed = questions
     incoming = {}
- 
     if request.method == "POST":
         count = 0
         for i,j in request.form.lists():
@@ -149,55 +122,15 @@ def question():
                 if question["id"] == int(i):
                     if incoming[i] == question["answer"]:
                         count+=1            
-        iq = (100*count)/current_user.userinfo.age
+        iq = float("{0:.2f}".format((100*count)/current_user.userinfo.age))
         current_user.userinfo.iq = iq
         db.session.commit()
-        
-        print("*"*90)
-        print(current_user.userinfo.iq)
-       
-        print("This is the incoming data : {}".format(count))
         flash(f'Account Updated successfully','success')
         return redirect(url_for('users.result',score = count))
     return render_template('question.html', title = 'Question', proposed = questions)
+
 
 @users.route("/question/result/<score>")
 def result(score):
     return render_template("result.html", score = score)
 
-# @users.route("/test")
-def parseData():
-    user = User.query.filter_by(username = "mob").first()
-    subjects = user.userinfo.subjects
-    userObj = {}
-    userObj["age"] = user.userinfo.age
-    userObj["IQ"] = float("{0:.2f}".format(user.userinfo.iq))
-    userObj["arm"] = user.userinfo.arm.name
-    subjectArr = {}
-    for subject in subjects:
-        subject_name = Subject.query.get(subject.subject_id)
-        subjectArr[subject_name.name] = subject.rating
-    userObj["subjects"] =  subjectArr
-    data = jsonify(userObj)
-    return data
-    
-
-dataform = []
-science_padding = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-@users.route("/test")
-def correctForm():
-    data = parseData().get_json()
-    print("*"*90)
- 
-    if data["arm"] == "Science":
-        for sub in data["subjects"]:
-            dataform = [data["age"],float(data["subjects"]["Mathematics"]),float(data["subjects"]["Biology"]),float(data["subjects"]["Physics"]),float(data["subjects"]["Chemistry"])]+science_padding
-    elif data["arm"] == "Art":
-        for sub in data["subjects"]:
-            dataform = [data["age"],data["IQ"],float(data["subjects"]["Mathematics"]),0.0,0.0,0.0,0.0,0.0,0.0,float(data["subjects"]["Government"]),float(data["subjects"]["English"]),float(data['subjects']["History"]),float(data['subjects']["CRK"])]
-    elif data["arm"] == "Commercial":
-        for sub in data["subjects"]:
-            dataform = [data["age"],data["IQ"],float(data['subjects']["Mathematics"]),0.0,0.0,0.0,float(data['subjects']["Accounting"]),float(data['subjects']["Commerce"]),float(data['subjects']["Economics"])]     
-    return data
-  
-    
---
+++ recommendation/utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from flask import jsonify
+from recommendation.models import User, Arm, Subjectrating, Subject, UserInfo
+
+def mapping(key):
+    mapping = pd.read_csv("../../mapping.csv")
+    result = mapping[mapping["CAREER INTEREST_enc"]== key]["CAREER INTEREST"].unique()[0]
+    return result
+
+
+def parseData(user):
+    subjects = user.userinfo.subjects
+    userObj = {}
+    userObj["age"] = float(user.userinfo.age)
+    userObj["IQ"] = float("{0:.2f}".format(user.userinfo.iq))
+    userObj["arm"] = user.userinfo.arm.name
+    subjectArr = {}
+    for subject in subjects:
+        subject_name = Subject.query.get(subject.subject_id)
+        subjectArr[subject_name.name] = subject.rating
+    userObj["subjects"] =  subjectArr
+    data = jsonify(userObj)
+    return data
+
+def correctForm(user: object):
+    dataform = []
+    science_padding = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    data = parseData(user).get_json()
+    if data["arm"] == "Science":
+        for sub in data["subjects"]:
+            dataform = [data["age"],data["IQ"],float(data["subjects"]["Mathematics"]),float(data["subjects"]["Biology"]),float(data["subjects"]["Physics"]),float(data["subjects"]["Chemistry"])]+science_padding
+    elif data["arm"] == "Art":
+        for sub in data["subjects"]:
+            dataform = [data["age"],data["IQ"],float(data["subjects"]["Mathematics"]),0.0,0.0,0.0,0.0,0.0,0.0,float(data["subjects"]["Government"]),float(data["subjects"]["English"]),float(data['subjects']["History"]),float(data['subjects']["CRK"])]
+    elif data["arm"] == "Commercial":
+        for sub in data["subjects"]:
+            dataform = [data["age"],data["IQ"],float(data['subjects']["Mathematics"]),0.0,0.0,0.0,float(data['subjects']["Accounting"]),float(data['subjects']["Commerce"]),float(data['subjects']["Economics"])]     
+    return dataform
