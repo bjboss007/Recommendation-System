282898b6ba8f4becadcd275598c045550ac1a4a3
Habib <habib.muhammad@longbridgetech.com>
2019-12-18 07:58:12 +0100

    House cleaning

--- recommendation/admin/forms.py
+++ recommendation/admin/forms.py
@@ -1,18 +1,20 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm,Form
 from flask_login import current_user
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, IntegerField, TextAreaField, FieldList, FormField
 from wtforms.widgets.html5 import NumberInput
 from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from recommendation.models import User 
+from recommendation.models import User, Option
 
 
 class Question(FlaskForm):
     question = TextAreaField("Question", validators=[DataRequired()])
     answer = StringField("answer", validators = [DataRequired()])
-    options = FieldList(FormField(Option), min_entries = 2 )
+    options = FieldList(FormField(Option), default = lambda: Option() )
     submit = SubmitField("submit")
     
-class Option(FlaskForm):
+class Option(Form):
     name = StringField("Option", validators = [DataRequired()])
 
+class CombinedForm(FlaskForm):
+    questions = FieldList(FormField(Question))
--- recommendation/admin/routes.py
+++ recommendation/admin/routes.py
@@ -1,17 +1,40 @@
 from flask import Blueprint, redirect, render_template, url_for, flash, request
 from flask_login import login_user, current_user,logout_user,login_required
-from recommendation.models import User
+from recommendation.models import User, Question
 from recommendation import bcrypt, db
+from recommendation.user.forms import LoginForm
+from .forms import Question
 import os
 
 admin = Blueprint('admin', __name__)
 
 
-# @admin.route("/login", methods = ["GET","POST"])
-# def login():
-#     if request.method == "POST":
-#         if request.form["username"] == "admin@gmail" and request.form["password"] == "password":
-#             login(admin)
+@admin.route("/admin/login", methods = ["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.email).first()
+        if user.Username == "admin@gmail.com":
+            login_user(user)
+        else:
+            flash(f'Login Unsuccessful, Please check your email and password ','danger')
+    return render_template('login.html', title ="Login", form= form)
+
+@admin.route("/admin/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('admin.login'))
+
+@admin.route("/admin/add-questions")
+def addQuestion():
+    question = Question.query.first()
+    if len(question.options) == 0:
+        flash("empty Phone provided")
     
+    form = Question()
+    if form.validate_on_submit():
+        form.populate_obj(question)
+        db.session.commit()
+        flash("Question Successfully save")
     
-     
+    return render_template('admin/question.html', title = "Add-question", form = form)
